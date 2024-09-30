19c5c3d3a245472b1b7daa6804cd9d3bb844772e
7orcas <js@7orcas.com>
2024-10-01 12:36:45 +1300

    initial

---
+++ Backend/Modules/Machines/IMachineService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Backend.Modules.Machines.Ent;
+
+namespace Backend.Modules.Machines
+{
+    public interface IMachineService
+    {
+        Task<List<Machine>> GetMachines();
+    }
+}
---
+++ Backend/Modules/Machines/MachineService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Backend.Modules.Machines.Ent;
+
+namespace Backend.Modules.Machines
+{
+    public class MachineService: IMachineService
+    {
+
+        public async Task<List<Machine>> GetMachines()
+        {
+            return await Task.Run(() =>
+            {
+                List <Machine> machines = new List<Machine>();
+
+    Thread.Sleep(2000);
+
+                machines.Add(new Machine { 
+                    Code = "M1",
+                    Description = "M1 Description",
+                });
+                machines.Add(new Machine
+                {
+                    Code = "M2",
+                    Description = "M2 Description",
+                });
+                return machines;
+            });
+        }
+
+
+
+    }
+}
