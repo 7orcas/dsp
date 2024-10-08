90f1969082059efda4fc2039cdac6a1553647dd7
7orcas <js@7orcas.com>
2024-10-08 18:14:55 +1300

    a

---
+++ Web/Web/wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿window.showAlert = function (message) {
+    alert(message);
+};
+
+window.simulateClick = function (elementId) {
+    console.log("simulateClick called: id=" + elementId);
+    var element = document.getElementById(elementId);
+    if (element) {
+        element.click();
+    }
+};
